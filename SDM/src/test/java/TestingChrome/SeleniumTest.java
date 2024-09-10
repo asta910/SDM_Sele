a5e6aa2499f8f147f004d8147c87a7a6a4558013
Yogesh Dadas <yogeshdadas1818@gmail.com>
2024-09-11 00:04:00 +0530

    Initial commit

---
+++ SDM/src/test/java/TestingChrome/SeleniumTest.java
@@ -0,0 +1,32 @@
+package TestingChrome;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.get("https://www.saucedemo.com/");
+        try{
+            Thread.sleep(4000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        System.out.println("Opened successfully");
+
+        driver.close();
+    }
+}
---
+++ SDM/src/test/java/TestingChrome/Test1.java
@@ -0,0 +1,10 @@
+package TestingChrome;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
